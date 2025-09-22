4b42b80a69688a3edef6a3e4385a6fa61bc5c596
J-millar99 <wlwogus33@gmail.com>
2025-09-22 15:49:54 +0900

    uuid

--- src/main/java/com/finalproject/springbackend/SpringbackendApplication.java
+++ src/main/java/com/finalproject/springbackend/SpringbackendApplication.java
@@ -3,7 +3,6 @@ package com.finalproject.springbackend;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import jakarta.annotation.PostConstruct;
